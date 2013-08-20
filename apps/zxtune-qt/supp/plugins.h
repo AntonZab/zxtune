3897da814f9c94c806d95e4bad191986505a2961
vitamin-caig <vitamin.caig@gmail.com>
2013-08-20 10:44:33 +0000

    vc90: adding missed files for qt5 building.
    git-svn-id: https://zxtune.googlecode.com/svn/branches/vc90@2588 e19a5227-2556-0410-816f-a35683127991

---
+++ apps/zxtune-qt/supp/plugins.h
@@ -0,0 +1,20 @@
+/*
+Abstract:
+  Qt plugins registering function
+
+Last changed:
+  $Id$
+
+Author:
+  (C) Vitamin/CAIG/2001
+
+  This file is a part of zxtune-qt application based on zxtune library
+*/
+
+#pragma once
+#ifndef ZXTUNE_QT_PLUGINS_H_DEFINED
+#define ZXTUNE_QT_PLUGINS_H_DEFINED
+
+void LoadStaticQtPlugins();
+
+#endif //ZXTUNE_QT_PLUGINS_H_DEFINED
---
+++ apps/zxtune-qt/supp/plugins_windows.cpp
@@ -0,0 +1,22 @@
+/*
+Abstract:
+  Qt plugins registering function for windows
+
+Last changed:
+  $Id$
+
+Author:
+  (C) Vitamin/CAIG/2001
+
+  This file is a part of zxtune-qt application based on zxtune library
+*/
+
+//local includes
+#include "plugins.h"
+//qt includes
+#include <QtCore/QtPlugin>
+
+void LoadStaticQtPlugins()
+{
+  Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin);
+}
